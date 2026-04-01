5338678c5ad6b676ec8965a8fb728201e11490d1
NelsonVieira77 <nelsonvieira328@gmail.com>
2026-04-01 10:22:05 -0300

    primeiro commiot

---
+++ src/test/java/com/logTrack/LogTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
